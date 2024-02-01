07d3b10a73705e269d7284e73bf2e112a159ca8d
Yuelin Xu <yuelin.xu@cispa.de>
2024-02-01 12:35:03 +0100

    for run

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name=RML
+#SBATCH --partition=a100  # 或者您可以选择其他可用的 GPU 分区
+#SBATCH --gres=gpu:4  # 请求 4 个 GPU
+#SBATCH --time=8:00:00  # 或者您需要的时间
+#SBATCH --output=job-%j.out
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=<yuelin.xu@cispa.de>
+
+# 运行您的 Python 脚本
+srun --container-image=pytorch/pytorch:1.11.0-cuda11.3-cudnn8-runtime \
+python ~/CISPA-home/OverfittingInRML/RML.py
+
