7816dabe294e6f74e81f78a18cde723c87378d4f
Yuelin Xu <yuelin.xu@cispa.de>
2024-02-01 12:50:35 +0100

    modified:   run.sh

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 
 #SBATCH --job-name=RML
 #SBATCH --partition=a100  # 或者您可以选择其他可用的 GPU 分区
-#SBATCH --gres=gpu:1  # 请求 4 个 GPU
-#SBATCH --time=12:00:00  # 或者您需要的时间
+#SBATCH --gres=gpu:4  # 请求 4 个 GPU
+#SBATCH --time=8:00:00  # 或者您需要的时间
 #SBATCH --output=job-%j.out
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=<yuelin.xu@cispa.de>
